9257d195d753f7984814aaa0c00c58d8b7ee0b59
Olga Polukarova <ol.venet@gmail.com>
2020-03-14 17:05:50 +0300

    mobile menu, media 768

--- src/js/template/_main.js
+++ src/js/template/_main.js
@@ -1,3 +1,19 @@
+//меню-бургер
+$(".burger").click(function () {
+  $(".mobile-menu").addClass("active");
+});
+
+
+$(".close").click(function () {
+  $(".mobile-menu").removeClass("active");
+});
+
+
+$(".mobile-menu__link").click(function () {
+  $(".mobile-menu").removeClass("active");
+});
+
+//accordeon
 var section = $('.accordeon__section');
 
 function toggleAccordion() {
